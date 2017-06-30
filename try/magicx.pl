f46d6aa3bd41717c60e80b1281c4c0b72f7e5766
monpeco <monpeco@gmail.com>
2017-06-30 16:17:39 +0000

    Add some of the magic of 'x'

---
+++ try/magicx.pl
@@ -0,0 +1,17 @@
+use strict;
+use warnings;
+
+print "print \"-\" x 40\n";
+print "-" x 40;
+
+print "\n\nprint 10 x 40;\n";
+print 10 x 40;
+
+print "\n\nprint 10 * 40;\n";
+print 10 * 40;
+
+print "\n\n\"/\\_\" x 20;\n";
+print "/\\_" x 20;
+
+
+print "\n";
