6228035b2a962e53c1351d3da3403d644a225c3a
monpeco <monpeco@gmail.com>
2017-07-12 18:01:22 +0000

    Add what is false and true in perl

--- try/conditions.pl
+++ try/conditions.pl
@@ -5,3 +5,26 @@ use warnings;
 if (1) { print "onion" }
 print "\n";
 
+## The do if construction
+do { print "onion" } if 1
+
+## The contrary
+do { print "onion" } unless 1
+
+## The same construction but with not instead 
+print "onion" if not 0
+
+
+## what is false :
+##   0 is false
+##   "0" is false
+##   0.0 is false
+##   undef is false
+##   Empty array () is false
+
+## So what is true ?
+##   " " is true
+##   "0.0" is true
+##   1 is true
+##   "banana" is true
+
